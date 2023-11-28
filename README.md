33c9527b598b30f3f73c25cc82b51605056e3a97
sahil kashetwar <sahilkashetwar24@gmail.com>
2023-11-28 11:11:41 +0530

    Readme + Simple Storage Contract

---
+++ README.md
@@ -0,0 +1,60 @@
+# Learning Solidity
+
+
+- Tools: https://remix.ethereum.org
+
+
+
+DataTypes: 
+ - bool
+ - string
+ - uint
+ - int
+ - struct
+ - [] // variable length
+ - [3] // array with defined length
+
+Contracts: 
+ - contract ContractName
+
+Declaration: 
+
+```sol
+
+uint256 public favNumber;
+
+People public person;
+
+struct People {
+  uint256 favNumber;
+  string name;
+}
+
+```
+
+Functions: 
+ - function fnName
+
+Storage type declaration: 
+ - memory : only for args
+ - calldata : only for args valid inside the function and get cleanup
+ - storage
+
+
+Importing: 
+ - import 
+
+```sol
+import './simpleStorage.sol'
+```
+
+Deploy Contracts: 
+ - Deploying contracts from other contracts.
+
+Inheritance: 
+ - is
+
+
+Overriding: 
+ - virtual : function that we can override
+ - override : function which works as overriden
---
+++ contracts/simpleStorage.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+  
+  uint256 public favNumber;
+  
+  // persons list array 
+  // need to remember the person index. 
+  People[] public personList;
+  
+  // mapping solves the above issue - can fetch the num based on name
+  
+  mapping(string => uint256) personToFavNum;
+  
+  // prepare structure for storing the person details.
+  struct People {
+    uint256 favNumber; 
+    string name;
+  }
+  
+  function setFavNumber(uint256 _favNumber) public {
+    favNumber = _favNumber;
+  }
+  
+  function retrieveFavNumber() public view returns(uint256) {
+    return favNumber;
+  }
+  
+  function addPerson(string calldata _name,  uint256 _favNumber ) public {
+    personList.push(People(_favNumber, _name)); 
+    personToFavNum[_name] = _favNumber;
+  }
+}
