c1ef65a710784f134b8f222768fa72e717974e73
sahil kashetwar <sahilkashetwar24@gmail.com>
2023-11-28 12:04:36 +0530

    crated simple storage contract list - sf fn to update and get value of contract instance.

--- contracts/storageFactory.sol
+++ contracts/storageFactory.sol
@@ -5,10 +5,33 @@ import "./simpleStorage.sol";
 
 contract StorageFactory {
   
-  SimpleStorage public simpleStorage;
+  SimpleStorage[] public simpleStorageList;
   
   function createStorage() public {
-    simpleStorage = new SimpleStorage();
+    SimpleStorage simpleStorage = new SimpleStorage();
+    simpleStorageList.push(simpleStorage);
   }
   
+  // updating the favNumber of different instance of simpleStorage;
+  
+  function SfStore(uint256 _simpleStorageIndex, uint256 _favNumber) public {
+    SimpleStorage simpleStorage = simpleStorageList[_simpleStorageIndex];
+    simpleStorage.setFavNumber(_favNumber);
+    
+    // we can reduce the code with single line like.
+    /* 
+      simpleStorageList[_simpleStorageIndex].setFavNumber(_favNumber);
+    */
+  }
+  
+  function SfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+    SimpleStorage simpleStorage = simpleStorageList[_simpleStorageIndex];
+    return simpleStorage.retrieveFavNumber();
+    
+    // we can reduce the code with single line like.
+    /* 
+      return simpleStorageList[_simpleStorageIndex].retrieveFavNumber();
+    */
+  }
+   
 }
